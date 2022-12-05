5399829b3d79b7fe9ddd5e67e1d937a783449e58
user <user@noreply.github.io>
2022-12-05 14:23:10 +0000

    use bind-dirs

--- install.sh
+++ install.sh
@@ -15,16 +15,17 @@ cd "${WORKDIR}"
 
 curl --proto '=https' -tlsv1.2 -sSfL "${REMOTE}/restrict-firewall" -o restrict-firewall
 curl --proto '=https' -tlsv1.2 -sSfL "${REMOTE}/sing-box.service" -o sing-box.service
-curl --proto '=https' -tlsv1.2 -sSfL "${REMOTE}/sing-box.json" -o sing-box.json
+curl --proto '=https' -tlsv1.2 -sSfL "${REMOTE}/50_sing-box.conf" -o 50_sing-box.conf
+curl --proto '=https' -tlsv1.2 -sSfL "${REMOTE}/config.json" -o config.json
 
-sed -i "s#10.139.1.0#${INET4_ADDR}#" sing-box.json
+sed -i "s#10.139.1.0#${INET4_ADDR}#" config.json
 
-sudo install -Dm644 -t /rw/config/proxy "${WORKDIR}/sing-box.json"
-sudo install -Dm644 -t /rw/config/proxy "${WORKDIR}/sing-box.service"
+sudo install -Dm644 -t /rw/bind-dirs/etc/sing-box "${WORKDIR}/config.json"
+sudo install -Dm644 -t /rw/bind-dirs/etc/systemd/system/ "${WORKDIR}/sing-box.service"
 sudo install -Dm755 -t /rw/config/qubes-firewall.d "${WORKDIR}/restrict-firewall"
+sudo install -Dm644 -t /rw/config/qubes-bind-dirs.d "${WORKDIR}/50_sing-box.conf"
 
 sudo cp /rw/config/rc.local /rw/config/rc.local.old
-echo 'cp /rw/config/proxy/sing-box.service /etc/systemd/system/sing-box.service' | sudo tee -a /rw/config/rc.local
 echo 'systemctl --no-block restart sing-box.service' | sudo tee -a /rw/config/rc.local
 
 url="$(curl -sSfL https://api.github.com/repos/SagerNet/sing-box/releases/latest | grep browser_download_url | grep linux-amd64.tar.gz | cut -d\" -f4)"
@@ -35,5 +36,5 @@ sudo install -Dm755 -t /rw/usrlocal/bin "${WORKDIR}/sing-box"
 curl --proto '=https' -tlsv1.2 -sSfL https://github.com/SagerNet/sing-geoip/releases/latest/download/geoip.db -o geoip.db
 curl --proto '=https' -tlsv1.2 -sSfL https://github.com/SagerNet/sing-geosite/releases/latest/download/geosite.db -o geosite.db
 
-sudo install -Dm644 -t /rw/config/proxy "${WORKDIR}/geoip.db"
-sudo install -Dm644 -t /rw/config/proxy "${WORKDIR}/geosite.db"
+sudo install -Dm644 -t /rw/usrlocal/share/sing-box "${WORKDIR}/geoip.db"
+sudo install -Dm644 -t /rw/usrlocal/share/sing-box "${WORKDIR}/geosite.db"
